c1ab0d76e0486917c211727ffde5a8660fd2c859
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-08-28 16:42:26 +0300

    create database model

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const app = express();
 //middleware
 app.use(express.json());
 
+const mongoose = require("mongoose");
+//configure mongoose
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  }
+);
+
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
